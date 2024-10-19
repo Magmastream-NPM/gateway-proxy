d09c420c0ad75c0b5d80bd7b562b539330c6be61
SxMAbel <abelpurnwasy1@gmail.com>
2024-10-19 18:58:59 -0400

    resuming? need to be fixed up

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const config = require("./config.json");
 const cluster = require("cluster");
 const os = require("os");
 const { getDiscordGatewayUrl } = require("./src/getDiscordGatewayUrl");
+const { delay } = require("./src/delay");
+const { Intents } = require("./src/intents");
 
 if (cluster.isMaster) {
 	const numCPUs = os.cpus().length;
@@ -21,6 +23,7 @@ if (cluster.isMaster) {
 	const wss = new WebSocket.Server({ port: config.port });
 	const shardConnections = new Map();
 	const availableShards = [...Array(config.totalShards).keys()];
+	const sessionInfo = new Map();
 
 	wss.on("connection", async (clientSocket) => {
 		console.log("Client connected to the proxy");
@@ -34,13 +37,46 @@ if (cluster.isMaster) {
 
 		try {
 			const discordWsUrl = await getDiscordGatewayUrl();
-			const discordSocket = new WebSocket(`${discordWsUrl}?v=10&encoding=json`);
+
+			await delay(shardId * 5500);
+
+			const connectToDiscord = (resume = false) => {
+				const params = new URLSearchParams({
+					v: "10",
+					encoding: "json",
+				});
+				if (resume) {
+					const session = sessionInfo.get(shardId);
+					if (session) {
+						params.append("session_id", session.sessionId);
+						params.append("seq", session.sequence.toString());
+					}
+				}
+				return new WebSocket(`${discordWsUrl}?${params}`);
+			};
+
+			let discordSocket = connectToDiscord();
+
 			shardConnections.set(shardId, { discordSocket, clientSocket });
 
 			// Handle Discord WebSocket events
 			discordSocket.on("message", (message) => {
 				try {
 					const jsonMessage = JSON.parse(message);
+					if (jsonMessage.op === 0) {
+						// Dispatch
+						if (jsonMessage.t === "READY") {
+							sessionInfo.set(shardId, {
+								sessionId: jsonMessage.d.session_id,
+								sequence: jsonMessage.s,
+							});
+						} else if (jsonMessage.s) {
+							const session = sessionInfo.get(shardId);
+							if (session) {
+								session.sequence = jsonMessage.s;
+							}
+						}
+					}
 					clientSocket.send(JSON.stringify(jsonMessage));
 				} catch (error) {
 					console.error("Failed to parse message from Discord:", error);
@@ -49,15 +85,22 @@ if (cluster.isMaster) {
 
 			discordSocket.on("close", (code, reason) => {
 				console.log(`Discord WebSocket for Shard ${shardId} closed: ${code} ${reason}`);
-				clientSocket.close();
-				shardConnections.delete(shardId);
-				availableShards.push(shardId);
+				if (code === 4000) {
+					// Unknown error, try to resume
+					discordSocket = connectToDiscord(true);
+				} else {
+					clientSocket.close();
+					shardConnections.delete(shardId);
+					sessionInfo.delete(shardId);
+					availableShards.push(shardId);
+				}
 			});
 
 			discordSocket.on("error", (error) => {
 				console.error(`Discord WebSocket for Shard ${shardId} error:`, error);
 				clientSocket.close();
 				shardConnections.delete(shardId);
+				sessionInfo.delete(shardId);
 				availableShards.push(shardId);
 			});
 
@@ -65,6 +108,25 @@ if (cluster.isMaster) {
 			clientSocket.on("message", (message) => {
 				try {
 					const jsonMessage = JSON.parse(message);
+					if (jsonMessage.op === 2) {
+						// Identify
+						const intents =
+							Intents.GUILDS |
+							Intents.GUILD_MEMBERS |
+							Intents.GUILD_MODERATION |
+							Intents.GUILD_EXPRESSIONS |
+							Intents.GUILD_INTEGRATIONS |
+							Intents.GUILD_INVITES |
+							Intents.GUILD_VOICE_STATES |
+							Intents.GUILD_MESSAGES |
+							Intents.MESSAGE_CONTENT |
+							Intents.GUILD_SCHEDULED_EVENTS |
+							Intents.AUTO_MODERATION_CONFIGURATION |
+							Intents.AUTO_MODERATION_EXECUTION |
+							Intents.DIRECT_MESSAGES;
+
+						jsonMessage.d.intents = intents;
+					}
 					discordSocket.send(JSON.stringify(jsonMessage));
 				} catch (error) {
 					console.error("Failed to parse message from client:", error);
@@ -75,6 +137,7 @@ if (cluster.isMaster) {
 				console.log(`Client for Shard ${shardId} disconnected`);
 				discordSocket.close();
 				shardConnections.delete(shardId);
+				sessionInfo.delete(shardId);
 				availableShards.push(shardId);
 			});
 
@@ -82,6 +145,7 @@ if (cluster.isMaster) {
 				console.error(`Client WebSocket for Shard ${shardId} error:`, error);
 				discordSocket.close();
 				shardConnections.delete(shardId);
+				sessionInfo.delete(shardId);
 				availableShards.push(shardId);
 			});
 		} catch (error) {
